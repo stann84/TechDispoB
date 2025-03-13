d6b3721be5aa06e72ec68a2eebd335820a531f0e
stan bomin <stann84@hotmail.com>
2025-03-13 11:58:01 +0100

    correction fusion

--- TechDispoB/Services/Implementations/HttpClientService.cs
+++ TechDispoB/Services/Implementations/HttpClientService.cs
@@ -11,8 +11,32 @@
 
             return new HttpClient(handler)
             {
-                BaseAddress = new Uri("https://37e1-2a01-e0a-1d4-b530-3449-b02c-66f9-aec1.ngrok-free.app/api/")
+                BaseAddress = new Uri(" https://11f7-2a01-e0a-1d4-b530-9bd-6465-dba0-6203.ngrok-free.app/api/")
             };
         }
+        //public static async Task<bool> SendFCMTokenAsync(string userId, string fcmToken)
+        //{
+        //    try
+        //    {
+        //        var client = CreateHttpClient();
+
+        //        var data = new Dictionary<string, string>
+        //{
+        //    { "userId", userId },
+        //    { "fcmToken", fcmToken }
+        //};
+
+        //        var content = new FormUrlEncodedContent(data);
+        //        var response = await client.PostAsync("Account/UpdateFCMToken", content);
+
+        //        return response.IsSuccessStatusCode;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine($"Erreur lors de l'envoi du token FCM: {ex.Message}");
+        //        return false;
+        //    }
+        //}
+
     }
 }
